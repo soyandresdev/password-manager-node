cb05c19b5935e339e3c4eba544f6fde30d432493
Andres Hernandez Lozano <soyandresdev@gmail.com>
2020-05-18 23:31:13 -0500

    [UPDATE]: FILE JS with Standard

--- index.js
+++ index.js
@@ -1,95 +1,94 @@
 #!/usr/bin/env node
-"use strict";
+'use strict'
 
-const minimist = require("minimist");
-const { createDb } = require("./lib/db");
+const minimist = require('minimist')
+const { createDb } = require('./lib/db')
 
-const argv = minimist(process.argv.slice(2));
+const argv = minimist(process.argv.slice(2))
 
-async function main() {
-  const db = await createDb();
-  const command = argv._.shift();
+async function main () {
+  const db = await createDb()
+  const command = argv._.shift()
   switch (command) {
-    case "users:create":
+    case 'users:create':
       try {
-        const { user, pass } = argv;
-        await db.createUser(user, pass);
-        console.log(`${user} created`);
+        const { user, pass } = argv
+        await db.createUser(user, pass)
+        console.log(`${user} created`)
       } catch (err) {
-        throw new Error("Cannot create user");
+        throw new Error('Cannot create user')
       }
-      break;
-    case "users:list":
+      break
+    case 'users:list':
       try {
-        const result = await db.listUsers();
-        console.log(result);
-        if (!result || !result.users || !result.users.length)
-          return console.log("No users found");
+        const result = await db.listUsers()
+        console.log(result)
+        if (!result || !result.users || !result.users.length) { return console.log('No users found') }
         result.users.forEach((u) => {
-          console.log(`- ${u.user}`);
-        });
-        console.log(`\tTotal: ${result.count}`);
+          console.log(`- ${u.user}`)
+        })
+        console.log(`\tTotal: ${result.count}`)
       } catch (error) {
-        throw new Error("Cannot list user");
+        throw new Error('Cannot list user')
       }
-      break;
-    case "secrets:create":
+      break
+    case 'secrets:create':
       try {
-        const { user, name, value } = argv;
-        await db.createSecret(user, name, value);
-        console.log(`Secret: ${name} created`);
+        const { user, name, value } = argv
+        await db.createSecret(user, name, value)
+        console.log(`Secret: ${name} created`)
       } catch (err) {
-        throw new Error("Cannot create secret");
+        throw new Error('Cannot create secret')
       }
-      break;
-    case "secrets:list":
+      break
+    case 'secrets:list':
       try {
-        const { user } = argv;
-        const secrets = await db.listSecrets(user);
+        const { user } = argv
+        const secrets = await db.listSecrets(user)
         secrets.forEach((s) => {
-          console.log(`- ${s.name}`);
-        });
+          console.log(`- ${s.name}`)
+        })
         if (secrets) {
-          console.log(`Secret: ${secrets.length}`);
+          console.log(`Secret: ${secrets.length}`)
         }
       } catch (err) {
-        console.log(err);
-        throw new Error("Cannot list secret");
+        console.log(err)
+        throw new Error('Cannot list secret')
       }
-      break;
-    case "secrets:get":
+      break
+    case 'secrets:get':
       try {
-        const { user, name } = argv;
-        const secret = await db.getSecret(user, name);
-        if (!secret) return console.log(`secret ${name} not found`);
-        console.log(`- ${name} = ${secret.value}`);
+        const { user, name } = argv
+        const secret = await db.getSecret(user, name)
+        if (!secret) return console.log(`secret ${name} not found`)
+        console.log(`- ${name} = ${secret.value}`)
       } catch (err) {
-        console.log(err);
-        throw new Error("Cannot get secret");
+        console.log(err)
+        throw new Error('Cannot get secret')
       }
-      break;
-    case "secrets:update":
+      break
+    case 'secrets:update':
       try {
-        const { user, name, value } = argv;
-        await db.updateSecret(user, name, value);
-        console.log(`secret ${name} updated`);
+        const { user, name, value } = argv
+        await db.updateSecret(user, name, value)
+        console.log(`secret ${name} updated`)
       } catch (err) {
-        throw new Error("Cannot update secret");
+        throw new Error('Cannot update secret')
       }
-      break;
-    case "secrets:delete":
+      break
+    case 'secrets:delete':
       try {
-        const { user, name } = argv;
-        await db.deleteSecret(user, name);
-        console.log(`secret ${name} deleted`);
+        const { user, name } = argv
+        await db.deleteSecret(user, name)
+        console.log(`secret ${name} deleted`)
       } catch (err) {
-        throw new Error("Cannot delete secret");
+        throw new Error('Cannot delete secret')
       }
-      break;
+      break
     default:
-      console.log(`Command not found: ${command}`);
-      break;
+      console.log(`Command not found: ${command}`)
+      break
   }
 }
 
-main().catch((err) => console.log(err));
+main().catch((err) => console.log(err))
